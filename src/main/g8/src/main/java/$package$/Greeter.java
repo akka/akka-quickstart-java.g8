525923bc3c7077008ce13b46d81fd16cc33cec89
Johan Andrén <johan@markatta.com>
2023-10-23 17:13:11 +0200

    Use records now that we can

--- src/main/g8/src/main/java/$package$/Greeter.java
+++ src/main/g8/src/main/java/$package$/Greeter.java
@@ -9,49 +9,8 @@ import java.util.Objects;
 // #greeter
 public class Greeter extends AbstractBehavior<Greeter.Greet> {
 
-  public static final class Greet {
-    public final String whom;
-    public final ActorRef<Greeted> replyTo;
-
-    public Greet(String whom, ActorRef<Greeted> replyTo) {
-      this.whom = whom;
-      this.replyTo = replyTo;
-    }
-  }
-
-  public static final class Greeted {
-    public final String whom;
-    public final ActorRef<Greet> from;
-
-    public Greeted(String whom, ActorRef<Greet> from) {
-      this.whom = whom;
-      this.from = from;
-    }
-
-// #greeter
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Greeted greeted = (Greeted) o;
-      return Objects.equals(whom, greeted.whom) &&
-              Objects.equals(from, greeted.from);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(whom, from);
-    }
-
-    @Override
-    public String toString() {
-      return "Greeted{" +
-              "whom='" + whom + '\'' +
-              ", from=" + from +
-              '}';
-    }
-// #greeter
-  }
+  public static record Greet(String whom, ActorRef<Greeted> replyTo) {}
+  public static record Greeted(String whom, ActorRef<Greet> from) {}
 
   public static Behavior<Greet> create() {
     return Behaviors.setup(Greeter::new);
@@ -67,9 +26,9 @@ public class Greeter extends AbstractBehavior<Greeter.Greet> {
   }
 
   private Behavior<Greet> onGreet(Greet command) {
-    getContext().getLog().info("Hello {}!", command.whom);
+    getContext().getLog().info("Hello {}!", command.whom());
     //#greeter-send-message
-    command.replyTo.tell(new Greeted(command.whom, getContext().getSelf()));
+    command.replyTo().tell(new Greeted(command.whom(), getContext().getSelf()));
     //#greeter-send-message
     return this;
   }
